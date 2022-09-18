47fe7ac23bcd2cd4ca1cc05a52c2992997e73839
peterstlang <peter.chriss@hotmail.com>
2022-09-19 00:47:14 +0200

    fixed problem
    nå får jeg alle priser og kvadratmeter fra listingene yeehaw

--- Skraper1.py
+++ Skraper1.py
@@ -14,12 +14,24 @@ url = "https://www.finn.no/realestate/homes/search.html?sort=PUBLISHED_DESC"
 
 site = get(url)
 
-print(site.content, "html.parser")
+#print(site.content, "html.parser")
 
 #teste
 bs = BeautifulSoup(site.content, "html.parser")
 
-for offer in bs.find_all('article', class_="relative overflow-hidden transition-all outline-none sf-ad-outline sf-ad-card rounded-8 mt-24 mx-16 mb-16 sm:mb-24 relative"):
-    print(offer)
-    break
+
+lst = []
+for offer in bs.find_all('div', class_='col-span-2 mt-16 sm:mt-4 flex justify-between sm:block space-x-12 font-bold'):
+    #print(offer.stripped_strings)
+    #print(offer)
+    tst = [text for text in offer.stripped_strings]
+    lst.append(tst)
+
+    
+    
+
+
+
+    
+    
 
