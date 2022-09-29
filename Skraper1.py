ba04f13c444e86f42aaa3074661da856b5a2f16c
peterstlang <peter.chriss@hotmail.com>
2022-09-29 14:06:00 +0200

    getting address

--- Skraper1.py
+++ Skraper1.py
@@ -21,19 +21,29 @@ bs = BeautifulSoup(site.content, "html.parser")
 
 
 lst = []
-for listing in bs.find_all('article', class_="relative overflow-hidden transition-all outline-none sf-ad-outline sf-ad-card rounded-8 mt-24 mx-16 mb-16 sm:mb-24 relative"):
+for listing in bs.find_all('article', 
+                           class_="relative overflow-hidden transition-all outline-none sf-ad-outline sf-ad-card rounded-8 mt-24 mx-16 mb-16 sm:mb-24 relative"):
     #print(offer.stripped_strings)
-    #print(offer)
+    print(listing.prettify())
+    #print("\n")
+    #print("\n")
+    
     #tst = [text for text in listing.stripped_strings]
     #lst.append(tst)
+    
     footer = listing.find('div', class_="col-span-2 mt-16 sm:mt-4 flex justify-between sm:block space-x-12 font-bold")
+    location = listing.find('span', class_="text-14 text-gray-500")
+    location = location.text
+    print(type(location))
+
     tst = [t for t in footer.stripped_strings]
     tst[1].replace('\\xa', ' ')
-    lst.append(tst)
+    m_square = tst[0]
+    price = tst[1]
+    print(m_square, price)
     
 
     
-
 
 
     
