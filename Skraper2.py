2069536e9a9a5c19c1ec778df7ae3032027dbd33
peterstlang <peter.chriss@hotmail.com>
2022-11-22 20:22:01 +0100

    Update Skraper2.py

--- Skraper2.py
+++ Skraper2.py
@@ -7,22 +7,35 @@ Created on Tue Nov 22 19:59:06 2022
 # plan is to use a link from "link_lager" in order to scrape
 
 # Objective of this script
+# needs functionality to get link from link_lager
+# creates a function to get desired link
+# needs to scrape said link and retrieve some information
+# information should be prisantydning, kvadratmeter, adresse and 
+# probably from which area it is, also unique code
+
+# The scraper should be able scrape all listings and stop when its done
+# possibly by if test?
+
+# areas should maybe even be divided into a loop?
+
+# Will begin by focusing on listings, as going in and checking for
+# each apartment might be tricky
+# Stop if you get no hits or page exceeds 50
+
+# add results in to a database
+# Creating and appending results to a db should probably be its own file
+# or function
+# Also needs a check to make sure its not already in db
+
+#
 
 # main function to run the script
 
 #importing "link_lager"
 from link_lager import createlink
 
-def link_to_scrape():
+if __name__ == "__main__":
     obj = createlink(areas=['Bøler', 'GAMLEOslo',
                             'Grorud', 'Røa', 'sentrum'], 
                      is_new_property=True)
-    return obj.get_link()
-
-def main():
-    link = link_to_scrape()
-    return link
-    
-
-if __name__ == "__main__":
-    link = main()
+    link = obj.get_link()
